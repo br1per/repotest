0f659316f18e8a022e8d55dfbaf1b93f31ba32d0
br1per <brunoperri99@gmail.com>
2025-10-02 20:13:05 +0200

    tt

--- test.py
+++ test.py
@@ -10,7 +10,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        if(debug):
+        if(debug=="True"):
           self.wfile.write(bytes("<html><body><h1> DEBUG-Hello from Python!</h1></body></html>", "utf-8"))
         else:
           self.wfile.write(bytes("<html><body><h1>Hello from Python!</h1></body></html>", "utf-8"))
